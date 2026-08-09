9015f27bdd73d432d84b4746004926546d565d03
Anirudh Chintaluri <155867540+anirudhc5@users.noreply.github.com>
2026-08-08 23:19:12 -0400

    Add: find-minimum-in-rotated-sorted-array - submission-3

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-3.py
@@ -0,0 +1,15 @@
+import math
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        # goal: find the value where the one before it is greater.
+        # what i can do: check at the center every time to see if the current one is greater than the last one. if so, move l to the right, otherwise move r to the left.
+        l, r = 0, len(nums) - 1
+        while l <= r and l >= 0 and r <= len(nums) - 1:
+            # print(l, r)
+            c = (l+r) // 2
+            if nums[c] <= nums[-1]:
+                if nums[c-1] > nums[c]: return nums[c]
+                else: r = c-1
+            else:
+                l = c + 1
+        return nums[0]
